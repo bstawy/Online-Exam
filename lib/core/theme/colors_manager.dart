8d645982c7fa1c86fcfbdda583837399fdeade57
bstawy <mohamed.bastawiie@gmail.com>
2024-10-12 12:41:58 +0300

    Update text styles in AppTheme and add lightBlue color in ColorsManager

--- lib/core/theme/colors_manager.dart
+++ lib/core/theme/colors_manager.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 
 class ColorsManager {
   static const Color blue = Color(0xFF02369C);
+  static const Color lightBlue = Color(0xFFDFE7F7);
   static const Color black = Color(0xFF0F0F0F);
-  static const Color grey = Color(0xFF878787);
-  static const Color lightGrey = Color(0xFFA6A6A6);
+  static const Color grey = Color(0xFF535353);
+  static const Color lightGrey = Color(0xFF878787);
+  static const Color lighterGrey = Color(0xFFA6A6A6);
   static const Color lightWhite = Color(0xFFF9F9F9);
 }
