aed0b29d84e37cc1b005a24ca5d9cd5f9e1fe168
bstawy <mohamed.bastawiie@gmail.com>
2024-10-23 19:52:29 +0300

    Add exan image in AssetsManager

--- lib/core/utils/assets_manager.dart
+++ lib/core/utils/assets_manager.dart
@@ -1,6 +1,8 @@
 class AssetsManager {
   static const String animations = 'assets/animations';
   static const String icons = 'assets/icons';
+  static const String images = 'assets/images';
+
   static const String loadingAnimation = '$animations/loading_animation.json';
   static const String successAnimation = '$animations/success_animation.json';
   static const String errorAnimation = '$animations/error_animation.json';
@@ -8,4 +10,6 @@ class AssetsManager {
   static const String homeNavBarIcon = '$icons/home_nav_bar_icon.svg';
   static const String resultsNavBarIcon = '$icons/result_nav_bar_icon.svg';
   static const String profileNavBarIcon = '$icons/profile_nav_bar_icon.svg';
+
+  static const String examIcon = '$images/exam_icon.png';
 }
